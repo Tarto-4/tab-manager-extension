9c8d1c2799451edf495cb95301626609ab76a178
Thato Mongwe <thatomongwe47@gmail.com>
2024-10-14 19:52:06 +0000

    reduce background.js to a minimal valid script

--- background.js
+++ background.js
@@ -1,17 +1,4 @@
-const saveTabsPeriodically = () => {
-    chrome.alarms.create('autoSave', { periodInMinutes: 5 });
-  };
-  
-  chrome.alarms.onAlarm.addListener((alarm) => {
-    if (alarm.name === 'autoSave') {
-      chrome.tabs.query({}, (tabs) => {
-        const tabData = tabs.map(tab => ({ title: tab.title, url: tab.url }));
-        chrome.storage.sync.set({ autoSavedSession: tabData });
-      });
-    }
-  });
-  
-  chrome.runtime.onInstalled.addListener(() => {
-    saveTabsPeriodically();
-  });
-  
+// background.js
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Tab Manager extension installed.');
+});
